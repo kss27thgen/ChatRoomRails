b4c368b49c782bcc8c1fa312e072126db4d91eb4
Oishi Yuikyo <s2008511@gmail.com>
2019-01-03 00:39:28 +0900

    Fix spelling of routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
   resources :messages, only: [:index]
-  resources :group, only: [:new, :create, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
   root 'messages#index'
 end
