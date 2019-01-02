70b91bdd1a0cbd8b8f9033628b85106170406e28
Oishi Yuikyo <s2008511@gmail.com>
2019-01-03 01:58:47 +0900

    groups#Indexの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update] do
+  resources :groups, only: [:new, :create, :edit, :update, :index] do
     resources :messages, only: [:index]
   end
   root 'messages#index'
