88cdf90c66e982e3ff04e8a0018120ace8158d7a
Oishi Yuikyo <s2008511@gmail.com>
2019-01-02 20:04:42 +0900

    Usersテーブルのgroupカラムをenum化

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum group: [:モー娘。, :アンジュルム, :JJ, :こぶしファクトリー, :つばきファクトリー, :BEYOOOOONS, :研修生 ]
 end
