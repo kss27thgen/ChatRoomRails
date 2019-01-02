b858c94e84f7299839e413b8bdbfd95be90d88d6
Oishi Yuikyo <s2008511@gmail.com>
2019-01-02 23:41:15 +0900

    User,group,group_userモデル間のアソシエーション、及びそれぞれのバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :groups, through: :group_users
+  has_many :group_users
 
   enum group: [:モー娘。, :アンジュルム, :JJ, :こぶしファクトリー, :つばきファクトリー, :BEYOOOOONS, :研修生 ]
 end
