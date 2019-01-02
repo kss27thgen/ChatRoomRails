08e2176bc25acb40e37200ca3eda1cf3a0a1757b
Oishi Yuikyo <s2008511@gmail.com>
2019-01-03 05:58:06 +0900

    messagesテーブルにgroup_idカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_02_163825) do
+ActiveRecord::Schema.define(version: 2019_01_02_204957) do
 
   create_table "group_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2019_01_02_163825) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "group_id"
+    t.index ["group_id"], name: "index_messages_on_group_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
@@ -59,5 +61,6 @@ ActiveRecord::Schema.define(version: 2019_01_02_163825) do
 
   add_foreign_key "group_users", "groups"
   add_foreign_key "group_users", "users"
+  add_foreign_key "messages", "groups"
   add_foreign_key "messages", "users"
 end
