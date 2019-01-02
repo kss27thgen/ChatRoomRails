bc5084017f056070b8b77fe0a9c3585796931717
Oishi Yuikyo <s2008511@gmail.com>
2019-01-02 20:26:25 +0900

    可能なところは@userをcurrent_userに置き換え

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,14 @@ class UsersController < ApplicationController
   end
 
   def update
-    current_user.update(user_params)
-    @user = current_user
+    if current_user.update(user_params)
+    else
+      render :edit
+    end
+
   end
 
-
   private
-
   def user_params
     params.require(:user).permit(:name, :email, :image, :group)
   end
