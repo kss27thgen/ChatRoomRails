4d9a1e7543d50c76f6266e8d08823923f055e84e
Oishi Yuikyo <s2008511@gmail.com>
2019-01-03 17:50:02 +0900

    メッセージ投稿アクションの追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,19 @@
 class MessagesController < ApplicationController
+  before_action :set_group
+
+  def create
+    @message = @group.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = "メッセージを入力してください"
+      render :index
+    end
+  end
 
   def index
-    @users = User.all
+    @message = Message.new
     @group = Group.find(params[:group_id])
     @messages = [
       "Lorem ipsum dolor sit amet, consectetur. ipsum dolor.",
@@ -16,16 +28,17 @@ class MessagesController < ApplicationController
       "Lorem ipsum dolor sit amet.",
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. -fin-"
     ]
-    @rooms = [
-      {name:"room1", message: "Lorem ipsum dolor sit amet"},
-      {name:"room2", message: "Lorem"},
-      {name:"room3", message: "Lorem ipsum dolor"},
-      {name:"room4", message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et velit, quas unde amet laborum fugiat recusandae aperiam quidem."},
-      {name:"room5", message: "Lorem ipsum dolor"},
-      {name:"room6", message: "Lorem ipsum dolor sit amet, consectetur."},
-      {name:"room7", message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, veniam, facilis! Aut accusamus eos excepturi rerum sit, eveniet ducimus quod. ipsum dolor sit amet, consectetur adipisicing elit. Neque quasi quia hic, illum facere itaque ut magni at maiores modi illo quod."},
-      {name:"room8", message: "Lorem ipsum dolor sit -fin-"}
-    ]
+
+  end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:text, :image).merge(user_id: current_user.id)
   end
 
 end
