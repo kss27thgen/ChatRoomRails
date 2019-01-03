bba54c349327126b26df64da0e95797acfea9ebf
Oishi Yuikyo <s2008511@gmail.com>
2019-01-03 17:49:13 +0900

    メッセージ送信機能のためのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
   resources :groups do
-    resources :messages, only: [:index]
+    resources :messages, only: [:index, :create]
   end
   root 'groups#index'
 end
