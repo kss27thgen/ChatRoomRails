9c517ccafa0bec7730c4f32987f4159d8e7a8508
Oishi Yuikyo <s2008511@gmail.com>
2019-01-15 00:39:46 +0900

    グループ外ユーザー投稿禁止機能

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,16 @@
 class MessagesController < ApplicationController
   before_action :set_group, only: [:create, :index, :destroy]
-  # before_action :join_first, only: [:create]
+  before_action :join_first, only: [:create]
 
   def create
+  # TODO グループ外ユーザーの登校禁止機能 完成度８割
   # パターン１
-    @message = @group.messages.new(message_params)
-    if @message.save
+    # @message = @group.messages.new(message_params)
+    # if @message.save
   # パターン２
     # if @group.messages.create(message_params)
   # パターン３(上２つならmessage_paramsに「group_id」のマージ不要)
-    # if !GroupUser.exists?(user_id: current_user.id, group_id: params[:group_id])
-    #   redirect_to group_messages_path(@group), notice: '投稿するにはルームに入りましょう'
-    # elsif @message = Message.create(message_params)
+    if @message = Message.create(message_params)
       respond_to do |format|
         format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
         format.json
@@ -22,8 +21,6 @@ class MessagesController < ApplicationController
       render :index
     end
   end
-  # TODO グループ外ユーザーの登校禁止
-
 
   def index
     @message = Message.new
@@ -37,18 +34,18 @@ class MessagesController < ApplicationController
   end
 
   private
-
   def set_group
     @group = Group.find(params[:group_id])
   end
 
-  # def join_first
-  #   if !GroupUser.exists?(user_id: current_user.id, group_id: params[:group_id])
-  #     redirect_to group_messages_path(@group), notice: '投稿するにはルームに入りましょう'
-  # end
+  def join_first
+    if !GroupUser.exists?(user_id: current_user.id, group_id: params[:group_id])
+      redirect_to group_messages_path(@group)
+      flash[:notice] = '投稿するにはルームに入りましょう'
+    end
+  end
 
   def message_params
     params.require(:message).permit(:text, :image).merge(user_id: current_user.id, group_id: params[:group_id])
   end
-
 end
