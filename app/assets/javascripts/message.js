b76eca171fdebd08484c754d91f9511f1f95f46f
Oishi Yuikyo <s2008511@gmail.com>
2019-01-15 01:56:41 +0900

    メッセージ削除機能の非同期化

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $(document).on('turbolinks:load',()=>{
   function buildHTML(message){
     const image = message.image? `<img src="${message.image}">` : "";
     const html = `<div class="chatBody">
-                  <h4 class="chatBody__name">${message.user_name}<span class="chatBody__belongsTo"> [${message.user_hello}]</span><span class="chatBody__time"> ${message.date}</span><a href="#" class="chatBody__delete"> x</a></h4>
+                  <h4 class="chatBody__name">${message.user_name}<span class="chatBody__belongsTo"> [${message.user_hello}]</span><span class="chatBody__time"> ${message.date}</span><a href="/groups/${message.group_id}/messages/${message.id}" class="chatBody__delete" data-method="delete"> x</a></h4>
                   <div class="chatBody__text">${message.text}</div>
                   <div class="chatBody__image">${image}</div>
                 </div>`
@@ -14,11 +14,12 @@ $(document).on('turbolinks:load',()=>{
     $('html,body').animate({ scrollTop: pos }, 2000);
   }
 
+// メッセージ投稿
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     e.stopPropagation();
     const formData = new FormData(this);
-    const url = $(this).attr('action');
+    let url = $(this).attr('action');
     $.ajax({
       url: url,
       type: 'post',
@@ -37,7 +38,22 @@ $(document).on('turbolinks:load',()=>{
     })
   });
 
+// 画面上へのスクロール
   $('.chatHeader__topBtn').on('click', ()=>{
     $('html,body').animate({ scrollTop: 2 }, 500);
   });
+
+// メッセージ削除
+  $('html').on('click', '.chatBody__delete', function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    $(this).parent().parent().hide();
+    let url = $(this).attr('href');
+    $.ajax({
+      url: url,
+      type: 'delete',
+      dataType: 'json',
+    })
+  });
+
 });
